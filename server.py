b7e2741192a2e19644fc3db7cf32c1945a7c0c98
scn22-ua <scn22@alu.ua.es>
2026-09-19 13:59:13 +0200

    aversiva

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 # server.py
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
 port = int(os.environ.get("PORT", 3000))
+
 class Handler(BaseHTTPRequestHandler):
- def do_GET(self):
- self.send_response(200)
- self.send_header("Content-Type", "text/plain")
- self.end_headers()
- self.wfile.write(b"Hello World desde Railway!")
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello World desde Railway!")
+
 print(f"Escuchando en el puerto {port}")
+
 HTTPServer(("0.0.0.0", port), Handler).serve_forever()
